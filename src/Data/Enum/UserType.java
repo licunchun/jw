f00c24f6687fa95fdba12965e14fc9a4f6a13d88
fengluoqiuwu <fengluo@mail.ustc.edu.cn>
2024-03-21 14:09:42 +0800

    更改

---
+++ src/Data/Enum/UserType.java
@@ -0,0 +1,16 @@
+package Data.Enum;
+
+public enum UserType {
+    None(""),Student("学生"),Teacher("教师"),Admin("管理员");
+
+    private final String name;
+
+    UserType(String name){
+        this.name=name;
+    }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
+}
