9ae8f6cb0a14a79d769178ca2d5ccd251d61973d
张逸安 <2332309318@qq.com>
2024-03-21 23:32:16 +0800

    增加了注册函数

--- src/Data/Student.java
+++ src/Data/Student.java
@@ -4,6 +4,8 @@ public class Student {
     String name;
     String account;
     String key;
+    String gender;
+    String major;
     String classes;
     String money;
     public Student(String info) {
