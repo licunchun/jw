137e47ba7e0815beb04d547eec43f488e006a28f
fengluoqiuwu <fengluo@mail.ustc.edu.cn>
2024-03-22 17:55:21 +0800

    更改

---
+++ src/GUI/util/StageUtil.java
@@ -0,0 +1,15 @@
+package GUI.util;
+
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class StageUtil {
+    //将窗口移至屏幕中间
+    public static void setCenter(Stage stage){
+        double x=(Toolkit.getDefaultToolkit().getScreenSize().getWidth()-stage.getWidth())/2;
+        double y=(Toolkit.getDefaultToolkit().getScreenSize().getHeight()-stage.getHeight())/2;
+        stage.setX(x);
+        stage.setY(y);
+    }
+}
