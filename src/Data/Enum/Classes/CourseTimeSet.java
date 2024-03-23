9af41ad279e2932a41a70d40f68cdbe5ba3d9c80
fengluoqiuwu <fengluo@mail.ustc.edu.cn>
2024-03-23 12:27:45 +0800

    更改

---
+++ src/Data/Enum/Classes/CourseTimeSet.java
@@ -0,0 +1,7 @@
+package Data.Enum.Classes;
+
+import java.util.Set;
+
+public class CourseTimeSet {
+    Set<CourseTime> courseTimeSet;
+}
---
+++ src/Data/Enum/Classes/Education.java
@@ -0,0 +1,19 @@
+package Data.Enum.Classes;
+
+public enum Education {
+    None(""),
+    Preparatory("预科"),
+    Undergraduate("本科"),
+    Graduate("研究生");
+
+    private final String name;
+
+    Education(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
