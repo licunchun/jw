17797605cd2187ae49a8267500ae46aa83923056
crl <2080758199@qq.com>
2024-03-23 13:42:22 +0800

    Regist

--- src/Data/Enum/Error/Regist.java
+++ src/Data/Enum/Error/Regist.java
@@ -7,6 +7,6 @@ public enum Regist {
     PasswordEmpty,//密码为空
     PasswordOverLength,//密码超长
     PasswordInvalidChar,//密码含非法字符
-    PasswordMatch,//两次输入的密码不一致
+    PasswordNotMatch,//两次输入的密码不一致
     Pass;//注册通过
 }
