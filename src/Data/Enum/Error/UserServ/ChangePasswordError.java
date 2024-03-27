01a6728ee2d09e40039e7773d29c731ba10c4a1d
fengluoqiuwu <163119756+fengluoqiuwu@users.noreply.github.com>
2024-03-27 12:31:48 +0800

    更改

---
+++ src/Data/Enum/Error/UserServ/ChangePasswordError.java
@@ -0,0 +1,11 @@
+package Data.Enum.Error.UserServ;
+
+public enum ChangePasswordError {
+    Success,//修改成功
+    IDNotFound,//未找到ID
+    EmptyInput,//originPassword，newPassword，newConfirmPassword有任何一个为空都返回这个
+    WrongOriginPassword,//原始密码错误
+    InvalidChar,//newPassword不合法字符
+    OverLength,//newPassword过长
+    NotMatch;//两次输入的密码不匹配
+}
