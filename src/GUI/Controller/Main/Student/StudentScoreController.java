45f4b192f55e293c30fa82f46507498b520fa06f
lcc17 <lcc17@mail.ustc.edu.cn>
2024-03-30 23:36:13 +0800

    fixed StudentScoreController

--- src/GUI/Controller/Main/Student/StudentScoreController.java
+++ src/GUI/Controller/Main/Student/StudentScoreController.java
@@ -1,10 +1,9 @@
 package GUI.Controller.Main.Student;
 
 import Data.Type.Student;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class StudentScoreController {
@@ -23,15 +22,11 @@ public class StudentScoreController {
     @FXML
     private TableView CourseTable;
     @FXML
-    ObservableList<String> Courses = FXCollections.observableArrayList();
-    @FXML
-    ObservableList<String> Periods = FXCollections.observableArrayList();
-    @FXML
-    ObservableList<String> Cridits = FXCollections.observableArrayList();
-    @FXML
-    ObservableList<String> GPAs = FXCollections.observableArrayList();
-    @FXML
-    ObservableList<String> Scores = FXCollections.observableArrayList();
+    TableColumn<String, String> Courses = new TableColumn<>();
+    TableColumn<String, String> Periods = new TableColumn<>();
+    TableColumn<String, String> Credits = new TableColumn<>();
+    TableColumn<String, String> GPAs = new TableColumn<>();
+    TableColumn<String, String> Scores = new TableColumn<>();
     public void init(Student student){
 
     }
