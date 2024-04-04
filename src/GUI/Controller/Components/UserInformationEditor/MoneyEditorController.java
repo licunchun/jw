5611f08d9ec791f4fa4861423c0c8d17b7f7ff35
fengluoqiuwu <163119756+fengluoqiuwu@users.noreply.github.com>
2024-04-04 23:18:05 +0800

    Update Manage User Page

--- src/GUI/Controller/Components/UserInformationEditor/MoneyEditorController.java
+++ src/GUI/Controller/Components/UserInformationEditor/MoneyEditorController.java
@@ -1,11 +1,53 @@
 package GUI.Controller.Components.UserInformationEditor;
 
+import GUI.Data.Enum.Error.Main.Components.UserServ.EditError;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class MoneyEditorController {//TODO
+import static Sevice.Main.Components.UserServ.UserServ.editMoney;
+
+public class MoneyEditorController {
+    @FXML
+    public TextField ChangedMoney;
+    @FXML
+    public Label Tips;
+    @FXML
+    public Button Confirm;
     private Stage stage;
     private String ID;
+    /*
+     * Functions
+     */
+    @FXML
+    private void initialize(){
 
+    }
+    @FXML
+    private void doConfirm(){
+        double money;
+        try{
+            money= Double.parseDouble(ChangedMoney.getText());
+        }catch(NumberFormatException e){
+            Tips.setText("金额不合法，请重新输入");
+            Tips.setVisible(true);
+            return;
+        }
+        switch(editMoney(ID,money)){
+            case EditError.IDNotFound:
+                Tips.setText("未找到ID，请重新登录！！");
+                Tips.setVisible(true);
+                break;
+            case EditError.Invalid:
+                Tips.setText("金额不合法，请重新输入");
+                Tips.setVisible(true);
+                break;
+            case Success:
+                stage.close();
+        }
+    }
     public void setStage(Stage stage) {
         this.stage = stage;
     }
--- src/GUI/Controller/Components/UserInformationEditor/PasswordEditorController.java
+++ src/GUI/Controller/Components/UserInformationEditor/PasswordEditorController.java
@@ -1,11 +1,46 @@
 package GUI.Controller.Components.UserInformationEditor;
 
+import GUI.Data.Enum.Error.Main.Components.UserServ.EditError;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class PasswordEditorController {//TODO
+import static Sevice.Main.Components.UserServ.UserServ.editName;
+import static Sevice.Main.Components.UserServ.UserServ.editPassword;
+
+public class PasswordEditorController {
+    @FXML
+    private TextField ChangedPassword;
+    @FXML
+    private Label Tips;
+    @FXML
+    private Button Confirm;
     private Stage stage;
     private String ID;
+    /*
+     * Functions
+     */
+    @FXML
+    private void initialize(){
 
+    }
+    @FXML
+    private void doConfirm(){
+        switch(editPassword(ID,ChangedPassword.getText())){
+            case EditError.IDNotFound:
+                Tips.setText("未找到ID，请重新登录！！");
+                Tips.setVisible(true);
+                break;
+            case EditError.Invalid:
+                Tips.setText("密码不合法，请重新输入");
+                Tips.setVisible(true);
+                break;
+            case Success:
+                stage.close();
+        }
+    }
     public void setStage(Stage stage) {
         this.stage = stage;
     }
