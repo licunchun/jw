e8f0427d0cfbf165a7905f250b686e9166b2b59d
fengluoqiuwu <163119756+fengluoqiuwu@users.noreply.github.com>
2024-04-07 23:40:20 +0800

    Add something in ClassesServ

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -24,8 +24,8 @@ public class Main extends Application {
         Main.stage = stage;
         stage.setOnHiding(e -> exit(0));
         stage.setResizable(false);
-        changeViews(stage, "/GUI/Window/Login/Login.fxml");
-        //changeViews(stage,"/GUI/Window/Main/Student/StudentMainMenu.fxml");
+        //changeViews(stage, "/GUI/Window/Login/Login.fxml");
+        changeViews(stage, "/GUI/Window/Main/Student/StudentMainMenu.fxml");
         //changeViews(stage,"/GUI/Window/Main/Teacher/TeacherMainMenu.fxml");
         //changeViews(stage,"/GUI/Window/Main/Admin/AdminMainMenu.fxml");
         //changeViews(stage,"/GUI/Window/Main/Common/Classes/ClassesChoosingPage.fxml");
