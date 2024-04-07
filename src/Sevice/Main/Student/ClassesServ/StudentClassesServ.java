f892c165c6dbaaf1bb65773e3ecea16f2edeac61
lcc17 <lcc17@mail.ustc.edu.cn>
2024-04-08 00:21:37 +0800

    added StudentClassesServ”

--- src/Sevice/Main/Student/ClassesServ/StudentClassesServ.java
+++ src/Sevice/Main/Student/ClassesServ/StudentClassesServ.java
@@ -1,9 +1,12 @@
 package Sevice.Main.Student.ClassesServ;
 
+import GUI.Data.DataPackage.Classes.Classes;
 import GUI.Data.DataPackage.Classes.ClassesSet;
 import GUI.Data.DataPackage.Classes.CourseCodeSet;
 import GUI.Data.Enum.Error.Main.Student.ClassesServ.DropClassesError;
 import GUI.Data.Enum.Error.Main.Student.ClassesServ.PickClassesError;
+
+import static Sevice.Main.Components.ClassServ.ClassesServ.getStudentGrade;
 
 public class StudentClassesServ {
     public static ClassesSet getStudentClassesSet(String ID) {
@@ -14,29 +17,78 @@ public class StudentClassesServ {
         return new CourseCodeSet();
     }//TODO
 
-    public static double getStudentTotalCredits(String ID) {
-        return 0;
-    }//TODO
+    public static double getStudentTotalCredits(String ID) {//返回总学分
+        ClassesSet classesSet = getStudentClassesSet(ID);
+        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+        double totalCredits = 0;
+        for(Classes studentClass : classesSetIterable) {
+            totalCredits = totalCredits + studentClass.getCredits();
+        }
+        return Math.round(totalCredits * 10) / 10.0;
+    }
 
-    public static double getStudentReceivedCredits(String ID) {
-        return 0;
-    }//TODO
+    public static double getStudentReceivedCredits(String ID) {//返回获得学分
+        ClassesSet classesSet = getStudentClassesSet(ID);
+        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+        double studentReceivedCredits = 0;
+        for(Classes studentClass : classesSetIterable) {
+            if(getStudentGrade(studentClass.getCode(), ID) >= 60) {
+                studentReceivedCredits = studentReceivedCredits + studentClass.getCredits();
+            }
+        }
+        return Math.round(studentReceivedCredits * 10) / 10.0;
+    }
 
-    public static double getStudentFailedCredits(String ID) {
-        return 0;
-    }//TODO
+    public static double getStudentFailedCredits(String ID) {//返回不及格学分
+        ClassesSet classesSet = getStudentClassesSet(ID);
+        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+        double studentFailedCredits = 0;
+        for(Classes studentClass : classesSetIterable) {
+            if(getStudentGrade(studentClass.getCode(), ID) < 60) {
+                studentFailedCredits = studentFailedCredits + studentClass.getCredits();
+            }
+        }
+        return Math.round(studentFailedCredits * 10) / 10.0;
+    }
 
-    public static int getStudentAverageGrade(String ID) {
-        return 0;
-    }//TODO
+    public static double getStudentAverageGrade(String ID) {//返回算术平均分
+        ClassesSet classesSet = getStudentClassesSet(ID);
+        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+        double studentAverageGrade = 0;
+        int countStudentClass = 0;//计算课程数
+        for(Classes studentClass : classesSetIterable) {
+            studentAverageGrade += getStudentGrade(studentClass.getCode(), ID);
+            countStudentClass ++;
+        }
+        studentAverageGrade /= countStudentClass;
+        return (Math.round(studentAverageGrade * 100) / 100.0);
+    }
 
-    public static int getStudentWeightedAverageGrade(String ID) {
-        return 0;
-    }//TODO
+    public static double getStudentWeightedAverageGrade(String ID) {//返回加权平均分
+        ClassesSet classesSet = getStudentClassesSet(ID);
+        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+        double studentWeightedAverageGrade = 0;
+        double countStudentCredits = 0;//计算总学分
+        for(Classes studentClass : classesSetIterable) {
+            studentWeightedAverageGrade += getStudentGrade(studentClass.getCode(), ID) * studentClass.getCredits();
+            countStudentCredits += studentClass.getCredits();
+        }
+        studentWeightedAverageGrade /= countStudentCredits;
+        return (Math.round(studentWeightedAverageGrade * 100) / 100.0);
+    }
 
-    public static double getStudentGPA(String classesCode, String ID) {
-        return 0;
-    }//TODO
+    public static double getStudentGPA(String classesCode, String ID) {//返回GPA
+        ClassesSet classesSet = getStudentClassesSet(ID);
+        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+        double studentGPA = 0;
+        double countStudentCredits = 0;//计算总学分
+        for(Classes studentClass : classesSetIterable) {
+            studentGPA += getStudentGPA(studentClass.getCode(), ID) * studentClass.getCredits();
+            countStudentCredits += studentClass.getCredits();
+        }
+        studentGPA /= countStudentCredits;
+        return (Math.round(studentGPA * 100) / 100.0);
+    }
 
     public static PickClassesError pickClasses(String studentID, String classesCode) {
         return PickClassesError.Success;
