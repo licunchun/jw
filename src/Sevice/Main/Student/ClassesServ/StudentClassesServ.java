e3d378e81b780da4b00440a446733623886099d0
fengluoqiuwu <163119756+fengluoqiuwu@users.noreply.github.com>
2024-04-08 10:31:09 +0800

    Delete some wrong function

--- src/Sevice/Main/Student/ClassesServ/StudentClassesServ.java
+++ src/Sevice/Main/Student/ClassesServ/StudentClassesServ.java
@@ -1,12 +1,9 @@
 package Sevice.Main.Student.ClassesServ;
 
-import GUI.Data.DataPackage.Classes.Classes;
 import GUI.Data.DataPackage.Classes.ClassesSet;
 import GUI.Data.DataPackage.Classes.CourseCodeSet;
 import GUI.Data.Enum.Error.Main.Student.ClassesServ.DropClassesError;
 import GUI.Data.Enum.Error.Main.Student.ClassesServ.PickClassesError;
-
-import static Sevice.Main.Components.ClassServ.ClassesServ.getStudentGrade;
 
 public class StudentClassesServ {
     public static ClassesSet getStudentClassesSet(String ID) {
@@ -17,81 +14,91 @@ public class StudentClassesServ {
         return new CourseCodeSet();
     }//TODO
 
-    public static double getStudentTotalCredits(String ID) {//返回总学分
-        ClassesSet classesSet = getStudentClassesSet(ID);
-        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
-        double totalCredits = 0;
-        for(Classes studentClass : classesSetIterable) {
-            totalCredits = totalCredits + studentClass.getCredits();
-        }
-        return Math.round(totalCredits * 10) / 10.0;
-    }
-
-    public static double getStudentReceivedCredits(String ID) {//返回获得学分
-        ClassesSet classesSet = getStudentClassesSet(ID);
-        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
-        double studentReceivedCredits = 0;
-        for(Classes studentClass : classesSetIterable) {
-            if(getStudentGrade(studentClass.getCode(), ID) >= 60) {
-                studentReceivedCredits = studentReceivedCredits + studentClass.getCredits();
-            }
-        }
-        return Math.round(studentReceivedCredits * 10) / 10.0;
-    }
-
-    public static double getStudentFailedCredits(String ID) {//返回不及格学分
-        ClassesSet classesSet = getStudentClassesSet(ID);
-        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
-        double studentFailedCredits = 0;
-        for(Classes studentClass : classesSetIterable) {
-            if(getStudentGrade(studentClass.getCode(), ID) < 60) {
-                studentFailedCredits = studentFailedCredits + studentClass.getCredits();
-            }
-        }
-        return Math.round(studentFailedCredits * 10) / 10.0;
-    }
-
-    public static double getStudentAverageGrade(String ID) {//返回算术平均分
-        ClassesSet classesSet = getStudentClassesSet(ID);
-        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
-        double studentAverageGrade = 0;
-        int countStudentClass = 0;//计算课程数
-        for(Classes studentClass : classesSetIterable) {
-            studentAverageGrade += getStudentGrade(studentClass.getCode(), ID);
-            countStudentClass ++;
-        }
-        studentAverageGrade /= countStudentClass;
-        return (Math.round(studentAverageGrade * 100) / 100.0);
-    }
-
-    public static double getStudentWeightedAverageGrade(String ID) {//返回加权平均分
-        ClassesSet classesSet = getStudentClassesSet(ID);
-        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
-        double studentWeightedAverageGrade = 0;
-        double countStudentCredits = 0;//计算总学分
-        for(Classes studentClass : classesSetIterable) {
-            studentWeightedAverageGrade += getStudentGrade(studentClass.getCode(), ID) * studentClass.getCredits();
-            countStudentCredits += studentClass.getCredits();
-        }
-        studentWeightedAverageGrade /= countStudentCredits;
-        return (Math.round(studentWeightedAverageGrade * 100) / 100.0);
-    }
-
-    public static double getStudentGPA(String ID) {//返回GPA
-        ClassesSet classesSet = getStudentClassesSet(ID);
-        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
-        double studentGPA = 0;
-        double countStudentCredits = 0;//计算总学分
-        for(Classes studentClass : classesSetIterable) {
-            studentGPA += getStudentClassGPA(studentClass.getCode(), ID) * studentClass.getCredits();
-            countStudentCredits += studentClass.getCredits();
-        }
-        studentGPA /= countStudentCredits;
-        return (Math.round(studentGPA * 100) / 100.0);
-    }
-    public static double getStudentClassGPA(String classesCode, String ID) {
+    public static double getStudentTotalCredits(String ID) {
+        //返回总学分by lcc,可以参考，但不能直接用，请对比文档或者全部重写
+//        ClassesSet classesSet = getStudentClassesSet(ID);
+//        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+//        double totalCredits = 0;
+//        for(Classes studentClass : classesSetIterable) {
+//            totalCredits = totalCredits + studentClass.getCredits();
+//        }
+//        return Math.round(totalCredits * 10) / 10.0;
         return 0;
     }//TODO
+
+    public static double getStudentReceivedCredits(String ID) {
+        //返回获得学分by lcc,可以参考，但不能直接用，请对比文档或者全部重写
+//        ClassesSet classesSet = getStudentClassesSet(ID);
+//        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+//        double studentReceivedCredits = 0;
+//        for(Classes studentClass : classesSetIterable) {
+//            if(getStudentGrade(studentClass.getCode(), ID) >= 60) {
+//                studentReceivedCredits = studentReceivedCredits + studentClass.getCredits();
+//            }
+//        }
+//        return Math.round(studentReceivedCredits * 10) / 10.0;
+        return 0;
+    }//TODO
+
+    public static double getStudentFailedCredits(String ID) {
+        //返回不及格学分by lcc,可以参考，但不能直接用，请对比文档或者全部重写
+//        ClassesSet classesSet = getStudentClassesSet(ID);
+//        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+//        double studentFailedCredits = 0;
+//        for(Classes studentClass : classesSetIterable) {
+//            if(getStudentGrade(studentClass.getCode(), ID) < 60) {
+//                studentFailedCredits = studentFailedCredits + studentClass.getCredits();
+//            }
+//        }
+//        return Math.round(studentFailedCredits * 10) / 10.0;
+        return 0;
+    }//TODO
+
+    public static double getStudentAverageGrade(String ID) {
+        //返回算术平均分by lcc,可以参考，但不能直接用，请对比文档或者全部重写
+//        ClassesSet classesSet = getStudentClassesSet(ID);
+//        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+//        double studentAverageGrade = 0;
+//        int countStudentClass = 0;//计算课程数
+//        for(Classes studentClass : classesSetIterable) {
+//            studentAverageGrade += getStudentGrade(studentClass.getCode(), ID);
+//            countStudentClass ++;
+//        }
+//        studentAverageGrade /= countStudentClass;
+//        return (Math.round(studentAverageGrade * 100) / 100.0);
+        return 0;
+    }//TODO
+
+    public static double getStudentWeightedAverageGrade(String ID) {
+        //返回加权平均分by lcc,可以参考，但不能直接用，请对比文档或者全部重写
+//        ClassesSet classesSet = getStudentClassesSet(ID);
+//        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+//        double studentWeightedAverageGrade = 0;
+//        double countStudentCredits = 0;//计算总学分
+//        for(Classes studentClass : classesSetIterable) {
+//            studentWeightedAverageGrade += getStudentGrade(studentClass.getCode(), ID) * studentClass.getCredits();
+//            countStudentCredits += studentClass.getCredits();
+//        }
+//        studentWeightedAverageGrade /= countStudentCredits;
+//        return (Math.round(studentWeightedAverageGrade * 100) / 100.0);
+        return 0;
+    }//TODO
+
+    public static double getStudentGPA(String ID) {
+        //返回GPA by lcc,可以参考，但不能直接用，请对比文档或者全部重写
+//        ClassesSet classesSet = getStudentClassesSet(ID);
+//        Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
+//        double studentGPA = 0;
+//        double countStudentCredits = 0;//计算总学分
+//        for(Classes studentClass : classesSetIterable) {
+//            studentGPA += getStudentClassGPA(studentClass.getCode(), ID) * studentClass.getCredits();
+//            countStudentCredits += studentClass.getCredits();
+//        }
+//        studentGPA /= countStudentCredits;
+//        return (Math.round(studentGPA * 100) / 100.0);
+        return 0;
+    }//TODO
+
     public static PickClassesError pickClasses(String studentID, String classesCode) {
         return PickClassesError.Success;
     }//TODO
