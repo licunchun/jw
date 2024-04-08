58cc9bba356cb0daf96d5d024ef92fe428a98b12
fengluoqiuwu <163119756+fengluoqiuwu@users.noreply.github.com>
2024-04-08 10:34:34 +0800

    Some update

--- src/GUI/Controller/Main/Student/StudentScoreController.java
+++ src/GUI/Controller/Main/Student/StudentScoreController.java
@@ -2,7 +2,6 @@ package GUI.Controller.Main.Student;
 
 import GUI.Data.DataPackage.Classes.Classes;
 import GUI.Data.DataPackage.Classes.ClassesSet;
-import GUI.Data.DataPackage.Classes.CourseCodeSet;
 import GUI.Data.DataPackage.Classes.StudentCourseScoreTable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,8 +10,16 @@ import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 
-import static Sevice.Main.Components.ClassServ.ClassesServ.*;
-import static Sevice.Main.Student.ClassesServ.StudentClassesServ.*;
+import static Sevice.Main.Components.ClassServ.ClassesServ.getStudentGPA;
+import static Sevice.Main.Components.ClassServ.ClassesServ.getStudentGrade;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentAverageGrade;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentClassesSet;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentCourseCodeSet;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentFailedCredits;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentGPA;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentReceivedCredits;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentTotalCredits;
+import static Sevice.Main.Student.ClassesServ.StudentClassesServ.getStudentWeightedAverageGrade;
 
 public class StudentScoreController {
     @FXML
@@ -41,12 +48,15 @@ public class StudentScoreController {
     private Label ArithmeticAverageScore;//算数平均分
     private ObservableList<StudentCourseScoreTable> data = FXCollections.observableArrayList();//用于表格的展示的ObservableList
     private String ID;
+
     public void setID(String ID) {
         this.ID = ID;
     }
+
     public void initialize() {
         loadTable();
     }
+
     public void flush() {
         data = getStudentCourseCodeSet(ID).toGradeObservableList();
         TotalCredits.setText(Double.toString(getStudentTotalCredits(ID)));
@@ -56,6 +66,7 @@ public class StudentScoreController {
         WeightAverageScore.setText(Double.toString(getStudentWeightedAverageGrade(ID)));
         ArithmeticAverageScore.setText(Double.toString(getStudentAverageGrade(ID)));
     }
+
     private void loadTable() {
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
         periodColumn.setCellValueFactory(cellData -> cellData.getValue().periodProperty().asObject());
@@ -66,7 +77,7 @@ public class StudentScoreController {
         ClassesSet classesSet = getStudentClassesSet(ID);
         Iterable<Classes> classesSetIterable = classesSet.getClassesIterable();
 
-        for(Classes studentClass : classesSetIterable) {
+        for (Classes studentClass : classesSetIterable) {
             String code = studentClass.getCode();
             int score = getStudentGrade(code, ID);
             double GPA = getStudentGPA(code, ID);
