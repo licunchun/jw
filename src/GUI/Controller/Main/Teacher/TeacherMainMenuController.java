35fc3bd947940cb6a6f1fd417b77a7e095692e60
fengluoqiuwu <163119756+fengluoqiuwu@users.noreply.github.com>
2024-04-11 12:59:55 +0800

    Some update

--- src/GUI/Controller/Main/Teacher/TeacherMainMenuController.java
+++ src/GUI/Controller/Main/Teacher/TeacherMainMenuController.java
@@ -202,4 +202,8 @@ public class TeacherMainMenuController {
     private void reloadPage() {
         openMainPage(stage, UserType.Teacher, ID);
     }
+
+    public TabPane getMainMenuTabPane() {
+        return mainMenuTabPane;
+    }
 }
