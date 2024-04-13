47069dd268f31d47116b9346222c1bc6cd15a0eb
lcc17 <lcc17@mail.ustc.edu.cn>
2024-04-13 15:23:07 +0800

    finished initialize

--- src/GUI/Controller/Main/Common/Classes/ClassesSchedulePageController.java
+++ src/GUI/Controller/Main/Common/Classes/ClassesSchedulePageController.java
@@ -1,10 +1,12 @@
 package GUI.Controller.Main.Common.Classes;
 
+import GUI.Data.DataPackage.Classes.StudentCourseScoreTable;
+import GUI.Data.DataPackage.Classes.TimeTable;
 import GUI.Data.Enum.User.UserType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 
@@ -13,8 +15,25 @@ public class ClassesSchedulePageController {//TODO
     private Label name;
     @FXML
     private Label userID;
-
-
+    @FXML
+    private TableView<TimeTable> timeTable;
+    @FXML
+    private TableColumn<TimeTable, Integer> numberColumn;
+    @FXML
+    private TableColumn<TimeTable, String> MondayColumn;
+    @FXML
+    private TableColumn<TimeTable, String> TuesdayColumn;
+    @FXML
+    private TableColumn<TimeTable, String> WednesdayColumn;
+    @FXML
+    private TableColumn<TimeTable, String> ThursdayColumn;
+    @FXML
+    private TableColumn<TimeTable, String> FridayColumn;
+    @FXML
+    private TableColumn<TimeTable, String> SaturdayColumn;
+    @FXML
+    private TableColumn<TimeTable, String> SundayColumn;
+    private ObservableList<TimeTable> data = FXCollections.observableArrayList();//用于表格的展示的ObservableList
     private String ID;
     private UserType userType;
 
@@ -30,10 +49,18 @@ public class ClassesSchedulePageController {//TODO
 
         return contextMenu;
     }
-
+    public void initialize(){
+        loadTable();
+    }
     public void flush() {
-
+        initializeData();
     }//TODO
+    public void initializeData() {
+        for(int i = 1; i <= 13 ; i++) {
+            TimeTable newTimetable = new TimeTable(i);
+            data.add(newTimetable);
+        }
+    }
 
     public void setID(String ID) {
         this.ID = ID;
@@ -41,5 +68,17 @@ public class ClassesSchedulePageController {//TODO
 
     public void setUserType(UserType userType) {
         this.userType = userType;
+    }
+    public void loadTable() {
+        numberColumn.setCellValueFactory(cellData -> cellData.getValue().numberProperty().asObject());
+        MondayColumn.setCellValueFactory(cellData -> cellData.getValue().mondayProperty());
+        TuesdayColumn.setCellValueFactory(cellData -> cellData.getValue().tuesdayProperty());
+        WednesdayColumn.setCellValueFactory(cellData -> cellData.getValue().wednesdayProperty());
+        ThursdayColumn.setCellValueFactory(cellData -> cellData.getValue().thursdayProperty());
+        FridayColumn.setCellValueFactory(cellData -> cellData.getValue().fridayProperty());
+        SaturdayColumn.setCellValueFactory(cellData -> cellData.getValue().saturdayProperty());
+        SundayColumn.setCellValueFactory(cellData -> cellData.getValue().sundayProperty());
+
+        timeTable.setItems(data);
     }
 }
--- src/GUI/Data/DataPackage/Classes/TimeTable.java
+++ src/GUI/Data/DataPackage/Classes/TimeTable.java
@@ -1,4 +1,90 @@
 package GUI.Data.DataPackage.Classes;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class TimeTable {//TODO
+    private final SimpleIntegerProperty number;
+    private final SimpleStringProperty Monday;
+    private final SimpleStringProperty Tuesday;
+    private final SimpleStringProperty Wednesday;
+    private final SimpleStringProperty Thursday;
+    private final SimpleStringProperty Friday;
+    private final SimpleStringProperty Saturday;
+    private final SimpleStringProperty Sunday;
+
+    public int getNumber() {
+        return number.get();
+    }
+
+    public SimpleIntegerProperty numberProperty() {
+        return number;
+    }
+
+    public String getMonday() {
+        return Monday.get();
+    }
+
+    public SimpleStringProperty mondayProperty() {
+        return Monday;
+    }
+
+    public String getTuesday() {
+        return Tuesday.get();
+    }
+
+    public SimpleStringProperty tuesdayProperty() {
+        return Tuesday;
+    }
+
+    public String getWednesday() {
+        return Wednesday.get();
+    }
+
+    public SimpleStringProperty wednesdayProperty() {
+        return Wednesday;
+    }
+
+    public String getThursday() {
+        return Thursday.get();
+    }
+
+    public SimpleStringProperty thursdayProperty() {
+        return Thursday;
+    }
+
+    public String getFriday() {
+        return Friday.get();
+    }
+
+    public SimpleStringProperty fridayProperty() {
+        return Friday;
+    }
+
+    public String getSaturday() {
+        return Saturday.get();
+    }
+
+    public SimpleStringProperty saturdayProperty() {
+        return Saturday;
+    }
+
+    public String getSunday() {
+        return Sunday.get();
+    }
+
+    public SimpleStringProperty sundayProperty() {
+        return Sunday;
+    }
+
+    public TimeTable(int number) {
+        this.number = new SimpleIntegerProperty(number);
+        this.Monday = new SimpleStringProperty();
+        this.Tuesday = new SimpleStringProperty();
+        this.Wednesday = new SimpleStringProperty();
+        this.Thursday = new SimpleStringProperty();
+        this.Friday = new SimpleStringProperty();
+        this.Saturday = new SimpleStringProperty();
+        this.Sunday = new SimpleStringProperty();
+    }
 }
