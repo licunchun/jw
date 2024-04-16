6e84d9e3f1d6e1e17fb11d1c714c4a0aa61982fc
张逸安 <2332309318@qq.com>
2024-04-16 16:58:16 +0800

    points

--- src/Data/Type/PointSet.java
+++ src/Data/Type/PointSet.java
@@ -3,17 +3,25 @@ package Data.Type;
 import java.util.ArrayList;
 
 public class PointSet {
-    ArrayList<Point> points;
+    public ArrayList<Point> points;
 
     public PointSet() {
         points = new ArrayList<>();
     }
-
     public Point get(int i) {
         return points.get(i);
     }
-
-    public int length(int i) {
+    public int length() {
         return points.size();
+    }
+    public void findAccount(String account) {
+        for (int i = 0; i < this.length(); i++) {
+            if (!this.get(i).account.equals(account)) points.remove(i--);
+        }
+    }
+    public void findCode(String code) {
+        for (int i = 0; i < this.length(); i++) {
+            if (!this.get(i).code.equals(code)) points.remove(i--);
+        }
     }
 }
--- src/TestBench.java
+++ src/TestBench.java
@@ -1,14 +1,38 @@
 import Data.DataBase;
 import Data.Type.ClassInfoSet;
+import Data.Type.Point;
+import Data.Type.PointSet;
 
 public class TestBench {
     public static void main(String[] args) {
+        /* ClassInfoSet.find() 系列函数用法
         DataBase dataBase = new DataBase();
 
         ClassInfoSet classInfoSet = dataBase.check();
         classInfoSet.findName("量子物理");
 
         System.out.println(classInfoSet.length());
+
+        dataBase.close();
+        */
+
+        DataBase dataBase = new DataBase();
+        PointSet pointSet;
+
+        dataBase.setPoint("011103.01", "PB23000000", "100");
+
+        pointSet = dataBase.points();
+        pointSet.findCode("011103.01");
+        for (int i = 0; i < pointSet.length(); i++) {
+            System.out.println("code: " + pointSet.get(i).code + ", account: " + pointSet.get(i).account + ", point: " + pointSet.get(i).point);
+        }
+
+        dataBase.setPoint("011103.01", "PB23000000", "null");
+
+        pointSet = dataBase.points();
+        for (int i = 0; i < pointSet.length(); i++) {
+            System.out.println("code: " + pointSet.get(i).code + ", account: " + pointSet.get(i).account + ", point: " + pointSet.get(i).point);
+        }
 
         dataBase.close();
     }
